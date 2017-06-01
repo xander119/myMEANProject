08c1d7eadd71b1271f31b09264de6ddf7340547e
xander119 <alexwang119@yahoo.com>
2017-06-01 20:57:27 +0100

    Add paper model

--- public/modules/products/config/products.client.routes.js
+++ public/modules/products/config/products.client.routes.js
@@ -5,6 +5,18 @@ angular.module('products').config(['$stateProvider',
 	function($stateProvider) {
 		// Products state routing
 		$stateProvider.
+		state('view-product', {
+			url: '/productview',
+			templateUrl: 'modules/products/views/view-product.client.view.html'
+		}).
+		state('list-product', {
+			url: '/productlist',
+			templateUrl: 'modules/products/views/list-product.client.view.html'
+		}).
+		state('edit-product', {
+			url: '/productedit',
+			templateUrl: 'modules/products/views/edit-product.client.view.html'
+		}).
 		state('create-product', {
 			url: '/product/create',
 			templateUrl: 'modules/products/views/create-product.client.view.html'
